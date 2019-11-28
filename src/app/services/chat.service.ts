11629c3cb5235a6f4a115cfac0f9ce50526ff68d
Arian Provenzano <aprovenzano@ms.local>
2019-11-28 11:12:12 -0300

    comments deleted on chat service

--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -18,33 +18,11 @@ export class ChatService {
   constructor(private http: HttpClient) { }
 
 
-  sendMessage(body : String){
-    console.log("hola");
-    console.log(body);
-    new Promise((resolve,reject) => {
-      this.http.post(environment.backend + 'messages/'+this.currentChannel,body).toPromise()
-      .then(() => {console.log("Todo ok")})
-      .catch(() => {console.log("Algo mal")});
-    })
-  }
-
   getMessages(id) {
-   /*  return new Promise((resolve, eject) => {
-
-      this.http.get(environment.backend + 'channels/' + id + '/messages').toPromise().then((messagesPromise) => {
-        resolve(messagesPromise);
-      });
-
-    }) */
     return this.http.get(environment.backend + 'channels/' + id + '/messages');
   }
 
-  public /* async */ loadMessages(){
-    /* var id = this.currentChannel;
-    var messages;
-    messages = await this.getMessages(id);
-    messages = this.mapMessages(messages);
-    return messages; */
+  public loadMessages(){
 
     var id = this.currentChannel;
     this.getMessages(id).subscribe((messages) => {
